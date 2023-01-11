2b2244963738b24a0d9ecd5d544081a7dae35878
Mayurini_priya <smpriya1219@gmail.com>
2023-01-11 21:30:32 +0530

    pie chart

--- lib/powerfuel_screens/OrderDelivery.dart
+++ lib/powerfuel_screens/OrderDelivery.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
@@ -10,6 +12,73 @@ class OrderDelivery extends StatefulWidget {
 }
 
 class _OrderDeliveryState extends State<OrderDelivery> {
+  double accepted = 0;
+  double pending = 0;
+  double cancelled = 0;
+  double completed = 0;
+
+  _fetchAccepted() async {
+    await FirebaseFirestore.instance
+        .collection('Station Orders')
+        .where("Status", isEqualTo: "Accepted")
+        .get()
+        .then((ds) {
+      setState(() {
+        accepted = ds.docs.length as double;
+        print(accepted);
+      });
+    }).catchError((e) {});
+  }
+
+  _fetchCancelled() async {
+    await FirebaseFirestore.instance
+        .collection('Station Orders')
+        .where("Status", isEqualTo: "Cancelled")
+        .get()
+        .then((ds) {
+      setState(() {
+        cancelled = ds.docs.length as double;
+        print(cancelled);
+      });
+    }).catchError((e) {});
+  }
+
+  _fetchPending() async {
+    await FirebaseFirestore.instance
+        .collection('Station Orders')
+        .where("Status", isEqualTo: "Pending")
+        .get()
+        .then((ds) {
+      setState(() {
+        pending = ds.docs.length as double;
+        print(pending);
+      });
+    }).catchError((e) {});
+  }
+
+  _fetchCompleted() async {
+    await FirebaseFirestore.instance
+        .collection('Station Orders')
+        .where("Status", isEqualTo: "Completed")
+        .get()
+        .then((ds) {
+      setState(() {
+        completed = ds.docs.length as double;
+        print(completed);
+      });
+    }).catchError((e) {});
+  }
+
+  @override
+  void initState() {
+    _fetchAccepted();
+    _fetchCancelled();
+    _fetchPending();
+    _fetchCompleted();
+    super.initState();
+  }
+
+  int touchedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -113,22 +182,195 @@ class _OrderDeliveryState extends State<OrderDelivery> {
                 ],
               ),
               SizedBox(
-                height: 40.0,
+                height: 140.0,
               ),
               //Now let's add the Table
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //Now let's set the pagination
                   SizedBox(
-                    height: 40.0,
+                    width: 300.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                        height: MediaQuery.of(context).size.height / 2,
+                        child: PieChart(
+                          PieChartData(
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+                            sectionsSpace: 0,
+                            centerSpaceRadius: 0,
+                            sections:
+                            showingSections(accepted, pending, cancelled, completed),
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    width: 200.0,
+                  ),
+                  Expanded(
+                    child: Column(children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xff0293ee),
+                              ),
+                              height: 15,
+                              width: 15,
+                            ),
+                            Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 15.0),
+                                  child: Text("Accepted"),
+                                )
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xfff8b250),
+                              ),
+                              height: 15,
+                              width: 15,
+                            ),
+                            Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 15.0),
+                                  child: Text("Pending"),
+                                )
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xff845bef),
+                              ),
+                              height: 15,
+                              width: 15,
+                            ),
+                            Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 15.0),
+                                  child: Text("Cancelled"),
+                                )
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xff13d38e),
+                              ),
+                              height: 15,
+                              width: 15,
+                            ),
+                            Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 15.0),
+                                  child: Text("Completed"),
+                                )
+                            )
+                          ],
+                        ),
+                      ),
+                    ],),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: 40.0,
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  List<PieChartSectionData> showingSections(
+      double accepted, double pending, double cancelled, double completed) {
+    return List.generate(4, (i) {
+      final fontSize = 16.0;
+      final radius = 200.0;
+
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: const Color(0xff0293ee),
+            value: accepted,
+            title: "$accepted%",
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff),
+            ),
+            badgePositionPercentageOffset: .98,
+          );
+        case 1:
+          return PieChartSectionData(
+            color: const Color(0xfff8b250),
+            value: pending,
+            title: "$pending%",
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff),
+            ),
+            badgePositionPercentageOffset: .98,
+          );
+        case 2:
+          return PieChartSectionData(
+            color: const Color(0xff845bef),
+            value: cancelled,
+            title: "$cancelled%",
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff),
+            ),
+            badgePositionPercentageOffset: .98,
+          );
+        case 3:
+          return PieChartSectionData(
+            color: const Color(0xff13d38e),
+            value: completed,
+            title: "$completed%",
+            radius: radius,
+            titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff),
+            ),
+            badgePositionPercentageOffset: .98,
+          );
+        default:
+          throw Exception('Oh no');
+      }
+    });
   }
 }
